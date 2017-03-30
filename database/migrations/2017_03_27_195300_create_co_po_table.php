af1bb9bb9c6e22078cca3ca9587b3ba6b45fb175
Muhammed Shasin <mohdshasin313@gmail.com>
2017-03-30 23:04:57 +0530

    some reference files

---
+++ database/migrations/2017_03_27_195300_create_co_po_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCoPoTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('co_po', function(Blueprint $table)
+		{
+			$table->string('co_id', 10);
+			$table->integer('po1');
+			$table->integer('po2');
+			$table->integer('po3');
+			$table->integer('po4');
+			$table->integer('po5');
+			$table->integer('po6');
+			$table->integer('po7');
+			$table->integer('po8');
+			$table->integer('po9');
+			$table->integer('po10');
+			$table->integer('po11');
+			$table->integer('po12');
+			$table->integer('pso13');
+			$table->integer('pso14');
+			$table->integer('pso15');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('co_po');
+	}
+
+}
