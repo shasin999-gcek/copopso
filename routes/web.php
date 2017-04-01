1a75b471068294042a569359e833bf916ca10ea9
Manish jayan <manishjayan@gmail.com>
2017-04-01 21:45:32 +0530

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,19 +9,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('form');
 });
-
 Route::post('/store', 'CoController@storecopo');
-
 Route::post('/copojust', 'CoController@store');
-
 Route::get('/copojust', function () {
     return view('co_po_matrix');
 });
-
 Route::get('/test', function() {
    return view('test');
+});
+Route::get('/subjectform', function () {
+	return view('subjectform');
 });
