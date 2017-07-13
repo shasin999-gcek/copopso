0a4577edc486008e5103f2216d7e4917c375b2ec
liyanasahir <liyanaliasahir@gmail.com>
2017-07-13 20:28:10 +0530

    Merge conflicts

--- app/Http/Controllers/MarksController.php
+++ app/Http/Controllers/MarksController.php
@@ -18,7 +18,6 @@ class MarksController extends Controller
     }
 
      
-
     public function store(Request $request)
     {   
     
@@ -27,37 +26,32 @@ class MarksController extends Controller
         {
           upload::truncate();
           return redirect()->back();    
-
         }
         elseif ($request->has('save')) {
               session()->flash('success', "Succesfully uploaded!");
               return redirect()->back();
         } 
       
-
         //get file
         $upload=$request->file('upload-file');
         if (!$upload) {
           session()->flash('danger', "PLEASE SELECT A FILE");
           return redirect()->back();
         }
-
         $validator = Validator::make(['file'=>$upload,'extension'=>strtolower($upload->getClientOriginalExtension()),],['file'=>'required','extension'=>'required|in:csv',]);
         
         if ($validator->fails()) {
           session()->flash('danger', "INVALID FILE!!");
           return redirect()->back();          
         }  
-
         $filePath=$upload->getRealPath();
         $file=fopen($filePath, 'r');
-
         
-        $header= fgetcsv($file);
+        $header1= fgetcsv($file);
+        $header2 =fgetcsv($file);
+        $header = array_replace($header1,array(0 => $header2[0] , 1 => $header2[1]));
         
         //open and read
-
-
         $escapedHeader=[];
         
         //validate
@@ -66,15 +60,14 @@ class MarksController extends Controller
             $escapedItem=preg_replace('/[^a-z0-9]/', '', $lheader);
             array_push($escapedHeader, $escapedItem);
         }
-
         //looping through other columns
         while($columns=fgetcsv($file))
         {
             if($columns[0]=="")
             {
+                dd("hi");
                 continue;
             }
-
            $data= array_combine($escapedHeader, $columns);
            
            //Displaying error if a blank field is found
@@ -87,7 +80,6 @@ class MarksController extends Controller
                return redirect()->back();
              }
            }
-
            // Table update
            $rollno=$data['rollno'];
            $name=$data['name'];
@@ -97,7 +89,6 @@ class MarksController extends Controller
            $a2=$data['a2'];
            $i=$data['i'];
            $u=$data['u'];
-
            $Upload= upload::firstOrNew(['rollno'=>$rollno]);
            $Upload->name=$name;
            $Upload->t1=$t1;
@@ -107,14 +98,18 @@ class MarksController extends Controller
            $Upload->i=$i;
            $Upload->u=$u;
            $Upload->save();           
-
         }
        //To prevent deletion of temp file for displaying of csv
        move_uploaded_file($filePath, substr($filePath, 0,strlen($filePath)-4).'A.tmp');
        //To delete the copy of temp file
+       
+       //if(file_exists($filePath)){
+        //    unlink($filePath);
+       //}    
        unlink(substr($filePath, 0,strlen($filePath)-4).'A.tmp');
-       return  view('display',['id'=>substr($filePath, 0,strlen($filePath)-4).'A.tmp']);
+       //$filePath = substr($filePath, 0,strlen($filePath)-4).'A.tmp';
+       return  view('display',['id'=>$filePath]);
     }
-
-
 }
+
+  
