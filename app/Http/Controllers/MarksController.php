cce7bc5e1285f4ac47ea32ec14a612cfa08cd658
TheFirstMe <sourabhsubhod@gmail.com>
2017-07-14 15:45:50 +0300

    major changes for csv upload

--- app/Http/Controllers/MarksController.php
+++ app/Http/Controllers/MarksController.php
@@ -18,40 +18,45 @@ class MarksController extends Controller
     }
 
      
+
     public function store(Request $request)
     {   
     
         //To discard or save uploaded csv file
         if($request->has('discard'))
         {
-          upload::truncate();
           return redirect()->back();    
+
         }
-        elseif ($request->has('save')) {
-              session()->flash('success', "Succesfully uploaded!");
+        /*elseif ($request->has('save')) {
+              session()->flash('danger', "Some error occured");
               return redirect()->back();
-        } 
-      
+        } */
+
         //get file
         $upload=$request->file('upload-file');
         if (!$upload) {
           session()->flash('danger', "PLEASE SELECT A FILE");
           return redirect()->back();
         }
+
         $validator = Validator::make(['file'=>$upload,'extension'=>strtolower($upload->getClientOriginalExtension()),],['file'=>'required','extension'=>'required|in:csv',]);
         
         if ($validator->fails()) {
           session()->flash('danger', "INVALID FILE!!");
           return redirect()->back();          
         }  
+
         $filePath=$upload->getRealPath();
         $file=fopen($filePath, 'r');
+
         
         $header1= fgetcsv($file);
         $header2 =fgetcsv($file);
         $header = array_replace($header1,array(0 => $header2[0] , 1 => $header2[1]));
         
         //open and read
+
         $escapedHeader=[];
         
         //validate
@@ -60,6 +65,7 @@ class MarksController extends Controller
             $escapedItem=preg_replace('/[^a-z0-9]/', '', $lheader);
             array_push($escapedHeader, $escapedItem);
         }
+
         //looping through other columns
         while($columns=fgetcsv($file))
         {
@@ -68,10 +74,11 @@ class MarksController extends Controller
                 dd("hi");
                 continue;
             }
+
            $data= array_combine($escapedHeader, $columns);
            
            //Displaying error if a blank field is found
-           foreach ($data as $key => $value) {
+           /*foreach ($data as $key => $value) {
              if ($value=="") {
                $message = 'BLANK FIELD AT ' . $key . ' FOR STUDENT ' . $data['name'] . '. PLEASE UPLOAD A VALID CSV FILE';
                //blank field found.... deleting all data
@@ -79,7 +86,8 @@ class MarksController extends Controller
                session()->flash('danger',$message);
                return redirect()->back();
              }
-           }
+           }*/
+
            // Table update
            $rollno=$data['rollno'];
            $name=$data['name'];
@@ -89,6 +97,7 @@ class MarksController extends Controller
            $a2=$data['a2'];
            $i=$data['i'];
            $u=$data['u'];
+
            $Upload= upload::firstOrNew(['rollno'=>$rollno]);
            $Upload->name=$name;
            $Upload->t1=$t1;
@@ -98,18 +107,21 @@ class MarksController extends Controller
            $Upload->i=$i;
            $Upload->u=$u;
            $Upload->save();           
+
         }
        //To prevent deletion of temp file for displaying of csv
-       move_uploaded_file($filePath, substr($filePath, 0,strlen($filePath)-4).'A.tmp');
+       //move_uploaded_file($filePath, substr($filePath, 0,strlen($filePath)-4).'A.tmp');
        //To delete the copy of temp file
        
        //if(file_exists($filePath)){
         //    unlink($filePath);
        //}    
-       unlink(substr($filePath, 0,strlen($filePath)-4).'A.tmp');
+       //unlink(substr($filePath, 0,strlen($filePath)-4).'A.tmp');
        //$filePath = substr($filePath, 0,strlen($filePath)-4).'A.tmp';
-       return  view('display',['id'=>$filePath]);
+       //return  view('display',['id'=>$filePath]);
+      session()->flash('success', "Successfully uploaded!!");
+      return redirect()->back();
     }
-}
 
-  
+
+}
