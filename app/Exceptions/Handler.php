f44b5a5b4d4e134b05aabfca0b55c1b79f16408b
shasin999-gcek <mohdshasin313@gmail.com>
2017-07-18 16:00:50 +0530

    Removed Custom exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,10 +44,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+      /*
       if($exception instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException)
       {
         return response()->view('layouts.app');
       }
+      */
 
       return parent::render($request, $exception);
     }
