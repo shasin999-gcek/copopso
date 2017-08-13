ef5bc4547e6928ae1781d8b03cd825e9ac1f15d1
shasin999-gcek <mohdshasin313@gmail.com>
2017-08-13 17:31:15 +0530

    minor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
 
         <!-- Scripts -->
         <script>
