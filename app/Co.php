7722224a6bbab9f757620a7c2cd800cc4246cc5f
shasin999-gcek <mohdshasin313@gmail.com>
2017-09-10 01:26:20 +0530

    updated asset files

--- app/Co.php
+++ app/Co.php
@@ -9,9 +9,8 @@ use App\CoPo;
 class Co extends Model
 {
     //
- 
+
     protected $table = 'cos';
-    public $timestamps = false;
 
     protected $fillable = [
         'id', 'name', 'description', 'user_course_id'
