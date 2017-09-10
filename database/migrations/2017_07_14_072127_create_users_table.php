317c702d46aa2b0ccab2192eff607fb16ac780f6
shasin999-gcek <mohdshasin313@gmail.com>
2017-09-10 23:40:44 +0530

    minor bug fixes

--- database/migrations/2017_07_14_072127_create_users_table.php
+++ database/migrations/2017_07_14_072127_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration {
 			$table->string('email')->unique();
 			$table->string('password');
 			$table->string('department');
+			$table->boolean('is_admin')->default(0);
 			$table->string('remember_token', 100)->nullable();
 			$table->timestamps();
 		});
